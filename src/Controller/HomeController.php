cf96466415e84b1aaab757cf36009f102b4a371d
Nathan Olive <65222446+Natolive@users.noreply.github.com>
2024-10-08 21:44:45 +0200

    build home and health controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
+    #[Route('/', name: 'app_home')]
     public function index(): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DefaultController.php',
+            'message' => 'Welcome at home !',
         ]);
     }
 }
